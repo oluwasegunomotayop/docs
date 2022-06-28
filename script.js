2fa78e9fa85b771692e68edab9a7cfbc0d4da591
Oluwasegun Omotayo <oluwasegunomotayp@gmail.com>
2022-06-28 13:02:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+// toggle menu
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+let showingAlert = false;
+const interval = setInterval(() => {document.title = showingAlert
+    ? 'Portfolio':'Omotayo Segun';
+    showingAlert =!showingAlert}, 1000);
